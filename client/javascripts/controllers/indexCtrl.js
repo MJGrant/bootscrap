d147272783cb2b0a41021582fb2375694d04199b
Mandi Grant <mandi018@gmail.com>
2015-05-23 09:56:40 -0700

    Adding a custom styles file, made a responsive button, fixed the disney ride dropdown (doesn't do much yet), added an example of side by side columns, made the fish text centered

--- client/javascripts/controllers/indexCtrl.js
+++ client/javascripts/controllers/indexCtrl.js
@@ -1,6 +1,9 @@
 /**
  * Created by mjgrant on 5/20/15.
  */
-angular.module("bootscrapp").controller("indexCtrl", function() {
+angular.module("bootscrapp").controller("indexCtrl", function($scope) {
    console.log("index controller successful");
+
+    $scope.rides = ["Tower of Terror", "Space Mountain", "Splash Mountain", "Star Tours", "Tower of Terror is the best"];
+    $scope.ridesList = $scope.rides[0];
 });
