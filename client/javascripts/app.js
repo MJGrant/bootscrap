6f2b5fd430f5eb48622cb2c13c19b3fdbc6daf17
Mandi Grant <mandi018@gmail.com>
2015-05-24 16:40:39 -0700

    Added custom directives for validating the integer field and the dropdowns. Got the form working. Form has some simple error messaging that might be improved upon later.

--- client/javascripts/app.js
+++ client/javascripts/app.js
@@ -5,5 +5,39 @@ angular.module('bootscrapp', ['ngRoute'], function($routeProvider) {
         templateUrl: './index.html'
     });
 
-
 });
+
+//Custom validators
+angular.module("bootscrapp").directive('rideDropdown', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            ctrl.$validators.rideDropdown = function(modelValue, viewValue) {
+
+                if(scope.user.rideChoice1 !== viewValue) {
+                    return true;
+                }
+
+                return false;
+            };
+        }
+    };
+});
+
+var intRegex = /^[-+]?\d+$/;
+angular.module("bootscrapp").directive('integerCheck', function() {
+    return {
+        require: 'ngModel',
+        link: function(scope, elm, attrs, ctrl) {
+            console.log("log");
+            ctrl.$validators.integerCheck = function(modelValue, viewValue) {
+                if (intRegex.test(viewValue)) {
+                    return true;
+                }
+                //otherwise, it's invalid
+                return false;
+            };
+        }
+    };
+});
+
--- client/javascripts/controllers/indexCtrl.js
+++ client/javascripts/controllers/indexCtrl.js
@@ -4,6 +4,23 @@
 angular.module("bootscrapp").controller("indexCtrl", function($scope) {
    console.log("index controller successful");
 
-    $scope.rides = ["Tower of Terror", "Space Mountain", "Splash Mountain", "Star Tours", "Tower of Terror is the best"];
+    $scope.rides = ["Tower of Terror (East)",
+                    "Tower of Terror (West)",
+                    "Space Mountain",
+                    "Splash Mountain",
+                    "Star Tours",
+                    "It's a Small World"];
+
     $scope.ridesList = $scope.rides[0];
+
+    $scope.user = {
+        email:'',
+        fiveIntegers: '',
+        rideChoice1: '',
+        rideChoice2: ''
+    };
+
+    $scope.submitForm = function() {
+        alert("Sweet form! You are great at following directions.");
+    };
 });
